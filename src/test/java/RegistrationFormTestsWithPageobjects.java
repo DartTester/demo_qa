93048e91776ccaf3fed8de929c2c7de5708eb910
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-06-17 22:45:46 +0300

    + page objects

--- src/test/java/RegistrationFormTestsWithPageobjects.java
+++ src/test/java/RegistrationFormTestsWithPageobjects.java
@@ -18,29 +18,24 @@ public class RegistrationFormTestsWithPageobjects extends tests.TestBase {
         .setFirstName(firstName)
         .setLastName(lastName)
         .setEmail("dart@tester.com")
-        .setGender("Other");
-
-        $("#userNumber").setValue("1231231230");
-        page_one.setDateOfBirth("4","August","1993");
-        /*$("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("July");
-        $(".react-datepicker__year-select").selectOption("2008");
-        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();*/
-
-        $("#subjectsInput").sendKeys("Maths");
-        $("#subjectsInput").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("pic.png");
-
-        $("#currentAddress").setValue("Some street 1");
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
-
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstName), text(lastName),
-                text("alex@egorov.com"), text("Some address 1"), text("30 July,2008"));
+        .setGender("Other")
+        .setNumber("81233212112")
+        .setDateOfBirth("4","August","1993")
+        .setSubject("Maths")
+        .setHobbies("Sports")
+        .setPicture("pic.png")
+        .setAddress("Address1")
+        .setState("NCR")
+        .setCity("Delhi")
+        .clickSubmit()
+        .checkFormOpened("Thanks for submitting the form")
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", "dart@tester.com")
+                .checkResult("Date of Birth", "4 August,1993")
+                .checkResult("Subjects", "Maths")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "pic.png")
+                .checkResult("Address", "Address1")
+                .checkResult("State and City", "NCR Delhi");
     }
 }
